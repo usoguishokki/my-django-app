731b0eec4e3c921493e78ea9915a0ff5b353df69
unknown <Administrator@jp1052vs074.ad.toyota-shokki.co.jp>
2026-03-30 10:24:05 +0900

    chore: stop tracking generated files

--- staticfiles/js/mobileLedger/mobileLedger.js
+++
@@ -1,8 +0,0 @@
-import { asynchronousCommunication } from '../asyncCommunicator/asyncCommunicator.js';
-import { initializeLoadingScreen } from '../manager/loadingManager.js';
-import { UIManger } from '../manager/UIManger.js';
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    initializeLoadingScreen();
-});
